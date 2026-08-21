bb242734a1c6610f830cec75128672ea2f7a8377
Purnime Prathibha <pmpurnimeprathibha@gmail.com>
2026-08-21 23:59:38 +0530

    fix: restrict CORS origin to localhost:3000 and add Dijkstra optimizer unit tests

--- src/main/java/com/ambulance/dispatch_system/network_detection/controller/NetworkAnalysisController.java
+++ src/main/java/com/ambulance/dispatch_system/network_detection/controller/NetworkAnalysisController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/network")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class NetworkAnalysisController {
 
     private final NetworkAnalysisService networkAnalysisService;
